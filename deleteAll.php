ca25158f463c5432d2e996229a0b64ac405c0891
Abdullah Fathallah <114351000+fathallah7@users.noreply.github.com>
2025-02-05 22:59:12 +0200

    Update deleteAll.php

--- deleteAll.php
+++ deleteAll.php
@@ -1,22 +1,18 @@
 <?php
 
 include 'db.php';
-
-
-
-
+session_start();
 
 $query = "TRUNCATE TABLE users";
 $result = mysqli_query($conn, $query);
 
-
 if ($result) {
-    header("Location: index.php?msg=allDeleted");
+    header("Location: index.php");
+    $_SESSION['msg'] = "All Data Deleted";
     exit();
 } else {
     echo "Error truncating table: " . mysqli_error($conn);
 }
-
 
 mysqli_close($conn);
 ?>
