5a562f5313265b92c51ff01cb631b957db7abb10
Abdullah Fathallah <114351000+fathallah7@users.noreply.github.com>
2025-02-05 22:59:36 +0200

    Update deleteOne.php

--- deleteOne.php
+++ deleteOne.php
@@ -1,17 +1,17 @@
 <?php 
 
 include 'db.php';
-
+session_start();
 
 $id = $_GET['id'];
 $sql = " DELETE FROM `users` WHERE id = $id ";
 $result = mysqli_query($conn , $sql);
 if ($result) {
-    header("Location:index.php?msg=Record Deleted");
+    header("Location:index.php");
+    $_SESSION['msg'] = "One User Deleted";
 }
 else {
     echo "Failed :" . mysqli_error($conn);
 }
-
 
 ?>
